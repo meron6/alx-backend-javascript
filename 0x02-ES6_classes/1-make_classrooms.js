4f1c9ed2e7ef26430b2da0262968bc62d1ae5d1b
meron6 <khozameron5@gmail.com>
2024-07-25 13:12:21 -0700

    update

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,11 @@
-// 1-make_classrooms.js
-import ClassRoom from './0-classroom.js';
+import Classroom from './0-classroom';
 
-export default function initializeRooms() {
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
-  return [room1, room2, room3];
+function initializeRooms() {
+  return [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34),
+  ];
 }
+
+export default initializeRooms;
