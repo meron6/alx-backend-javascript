348a06d43f671e7df794233e0b4321fb052171da
iMLmn4UqfQjYxJe <khozameron5@gmail.com>
2024-07-30 19:16:29 +0000

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,33 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class StudentUtil {
-
-    // Method to get students by location
-    public static List<Student> getStudentsByLocation(List<Student> students, String city) {
-        List<Student> filteredStudents = new ArrayList<>();
-
-        if (students != null) {
-            for (Student student : students) {
-                if (student.getLocation().equals(city)) {
-                    filteredStudents.add(student);
-                }
-            }
-        }
-
-        return filteredStudents;
-    }
-
-    public static void main(String[] args) {
-        // Example usage
-        List<Student> students = new ArrayList<>();
-        students.add(new Student(1, "Guillaume", "San Francisco"));
-        students.add(new Student(2, "James", "Columbia"));
-        students.add(new Student(5, "Serena", "San Francisco"));
-
-        List<Student> studentsInSF = getStudentsByLocation(students, "San Francisco");
-        for (Student student : studentsInSF) {
-            System.out.println(student);
-        }
-    }
+export default function getStudentsByLocation(students, city) {
+  return students.filter(student => student.location === city);
 }
