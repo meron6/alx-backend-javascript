f0a3a5a61bf644b31fceeca26b12499db7018093
iMLmn4UqfQjYxJe <khozameron5@gmail.com>
2024-07-30 20:51:05 +0000

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 export default function groceriesList() {
-  const items = [
+  const values = [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
-    ['Bananas', 5],
+    ['Banana', 5],
   ];
 
-  return new Map(items);
+  return new Map(values);
 }
