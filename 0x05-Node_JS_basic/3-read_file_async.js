3f5ef273e74393636c6794bc4ebc115da0e96f2d
meron6 <khozameron5@gmail.com>
2024-09-17 02:51:02 -0700

    update

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,28 +3,36 @@ const { readFile } = require('fs');
 function countStudents(fileName) {
   const students = {};
   const fields = {};
-  let totalStudents = 0;
+  let length = 0;
   return new Promise((resolve, reject) => {
     readFile(fileName, (error, data) => {
       if (error) {
-        reject(new Error('Cannot load the database'));
+        reject(Error('Cannot load the database'));
       } else {
         const lines = data.toString().split('\n');
-        lines.forEach((line, index) => {
-          if (line) {
-            totalStudents++;
-            const [name, , , field] = line.split(',');
-            students[field] = students[field] || [];
-            students[field].push(name);
-            fields[field] = (fields[field] || 0) + 1;
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
           }
-        });
-        console.log(`Number of students: ${totalStudents - 1}`);
-        Object.entries(fields).forEach(([key, value]) => {
+        }
+        const l = length - 1;
+        console.log(`Number of students: ${l}`);
+        for (const [key, value] of Object.entries(fields)) {
           if (key !== 'field') {
             console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
           }
-        });
+        }
         resolve(data);
       }
     });
--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,28 +5,28 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 function countStudents(fileName) {
-  // Function implementation needed here
 }
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then((output) => {
-        res.end(`This is the list of our students\n${output}`);
-      })
-      .catch(() => {
-        res.statusCode = 404;
-        res.end('Cannot load the database');
-      });
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      res.end(outString);
+    }).catch(() => {
+      res.statusCode = 404;
+      res.end('Cannot load the database');
+    });
   }
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
