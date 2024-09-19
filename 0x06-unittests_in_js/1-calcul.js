7c3b3fe25671bfc73e18ad5bf43bb8947af102cd
meron6 <khozameron5@gmail.com>
2024-09-19 14:07:16 -0700

    update

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,30 @@
+/**
+ * Rounds parameters and performs arithmetic operations
+ * @param {string} type - The type of operation to perform
+ * @param {number} a - The first number
+ * @param {number} b - The second number
+ * @returns {number|string} The result of the arithmetic operation or 'Error'
+ */
+function calculateNumber(type, a, b) {
+  let result;
+  switch (type) {
+    case 'SUM':
+      result = Math.round(a) + Math.round(b);
+      break;
+    case 'SUBTRACT':
+      result = Math.round(a) - Math.round(b);
+      break;
+    case 'DIVIDE':
+      if (Math.round(b) === 0) {
+        result = 'Error';
+      } else {
+        result = Math.round(a) / Math.round(b);
+      }
+      break;
+    default:
+      result = 'Invalid operation';
+  }
+  return result;
+}
+
+module.exports = calculateNumber;
